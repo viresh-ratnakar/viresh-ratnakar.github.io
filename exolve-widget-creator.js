ec99f69a14504e820fca0213d957966016b038f7
Viresh Ratnakar <39968616+viresh-ratnakar@users.noreply.github.com>
2020-07-20 23:18:31 -0700

    Add files via upload

--- exolve-widget-creator.js
+++ exolve-widget-creator.js
@@ -33,19 +33,17 @@ if (typeof exolveWidgets === 'undefined') {
  * Expects an HTMLElement with id exolve-widget-placeholder to be present.
  *     (changes its id to something unique).
  * puzzleText: exolve spec.
- * origin: url of the site that is using ExolveWidget, or "*".
+ * url: url of exolve-widget.html
  * height: optional height (in px) of iframe.
  */
-function ExolveWidgetCreator(puzzleText, origin, height=1500) {
+function ExolveWidgetCreator(puzzleText, url, height=1500) {
   this.VERSION = 'Exolve v0.80 July 20 2020';
   this.id = "xlv" + Math.random().toString(36).substring(2, 15);
   this.puzzleText = puzzleText;
-  this.origin = origin;
   this.sendPuzzleToExolveWidget = () => {
     document.getElementById(
         "exolve-iframe-" + this.id).contentWindow.postMessage(
-            this.puzzleText,
-            this.origin);
+            this.puzzleText, "*");
   };
   this.receiveExolveReady = (event) => {
     if (event.data == "EXOLVE-READY") {
@@ -63,7 +61,7 @@ function ExolveWidgetCreator(puzzleText, origin, height=1500) {
   placeholder.id = "exolve-widget-" + this.id;
   placeholder.insertAdjacentHTML('beforeend', `
     <iframe
-      src="exolve-widget.html"
+      src="${url}"
       width="100%" height="${height}" allow="fullscreen"
       id="exolve-iframe-${this.id}" frameborder="0"
       style="display:block; border:none; margin-left:auto; margin-right:auto">
