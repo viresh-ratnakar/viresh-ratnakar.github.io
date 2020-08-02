53aa313d5abc0f290a3d4921e416726f852a6b9b
Viresh Ratnakar <39968616+viresh-ratnakar@users.noreply.github.com>
2020-08-02 01:22:13 -0700

    Add files via upload

--- exolve-m.js
+++ exolve-m.js
@@ -2694,6 +2694,7 @@ function restoreState() {
       if (c.indexOf(name) == 0) {
         state = c.substring(name.length, c.length).trim();
         index = parseState(state)
+        break
       }
     }
   }
