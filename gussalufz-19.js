042558391da4e79a8bf3fb3996e8df4b8d02261b
Viresh Ratnakar <39968616+viresh-ratnakar@users.noreply.github.com>
2020-09-08 16:13:24 -0700

    Add files via upload

--- gussalufz-19.js
+++ gussalufz-19.js
@@ -231,7 +231,7 @@ function customizeExolve(p) {
   lim.setAttributeNS(null, 'height', 60)
   lim.style.position = 'absolute'
   lim.style.left = '315px'
-  lim.style.top = '15px'
+  lim.style.top = '30px'
   lim.style.display = 'none'
   puz.questions.style.position = 'relative'
   puz.questions.appendChild(lim)
