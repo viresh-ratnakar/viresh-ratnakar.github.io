c4f01462b1682cb5ab46dbdb7ae893128424f029
Viresh Ratnakar <39968616+viresh-ratnakar@users.noreply.github.com>
2022-05-03 21:19:19 -0700

    Add files via upload

--- dev/webifi.js
+++ dev/webifi.js
@@ -387,8 +387,10 @@ Webifi.prototype.commandMatch = function(words, matchers) {
 
 Webifi.prototype.handleInputInput = function() {
   if (this.inputWaiter) {
+    this.appendToLog('', 'Debug: clearing timeout');
     clearTimeout(this.inputWaiter);
   }
+    this.appendToLog('', 'Debug: input [' + this.input.value + ']');
   this.inputWaiter = setTimeout(this.handleInputChange.bind(this), 2000);
 }
 
