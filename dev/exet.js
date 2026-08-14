e2f482dd154e6a12a14721990459fd2a716ba6d7
Viresh Ratnakar <viresh@gmail.com>
2026-08-14 12:55:09 -0700

    Contd. resize fix - 3

--- dev/exet.js
+++ dev/exet.js
@@ -4282,21 +4282,6 @@ Exet.prototype.resizeRHS = function() {
 }
 
 Exet.prototype.reposition = function() {
-  const vpd = this.puz.getViewportDim();
-  console.log('vpd = ' + vpd);
-  if (this.puz.squareDim < 31 && this.lastViewportDim &&
-      (vpd - this.lastViewportDim > 25)) {
-    console.log('last vpd = ' + this.lastViewportDim);
-    /**
-     * The window is substantially bigger than when we created the grid. Let's
-     * just force a redraw (we don't use Exolve's resizing because that would
-     * create a new puz.grid and we would have to take care of re-adding
-     * viablots and forcedLetters * to the reborn gridCell.cellGroup fields.
-     */
-    this.updatePuzzle();  /** revType = default 0 won't actually save */
-    return;
-  }
-  this.lastViewportDim = vpd;
   const elts = [this.xetTitle, this.xetTitleCTA,
                 this.xetSetter, this.xetSetterCTA, this.preamble];
   for (const elt of elts) {
@@ -4334,6 +4319,22 @@ Exet.prototype.reposition = function() {
     }
   }
   this.resizeRHS();
+
+  const vpd = this.puz.getViewportDim();
+  console.log('vpd = ' + vpd);
+  if (this.puz.squareDim < 31 && this.lastViewportDim &&
+      (vpd - this.lastViewportDim > 25)) {
+    console.log('last vpd = ' + this.lastViewportDim);
+    /**
+     * The window is substantially bigger than when we created the grid. Let's
+     * just force a redraw (we don't use Exolve's resizing because that would
+     * create a new puz.grid and we would have to take care of re-adding
+     * viablots and forcedLetters * to the reborn gridCell.cellGroup fields.
+     */
+    this.updatePuzzle();  /** revType = default 0 won't actually save */
+    return;
+  }
+  this.lastViewportDim = vpd;
 }
 
 Exet.prototype.lastTagOpener = function(s) {
